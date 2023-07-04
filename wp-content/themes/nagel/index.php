e38fb4419b4f0a7eac0ac8814b651e9838af6f56
wingo-dev <rukshanlancer@gmail.com>
2023-07-04 17:26:03 +0800

    new theme development start

---
+++ wp-content/themes/nagel/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
+<head>
+    <meta charset="<?php bloginfo('charset');?>">
+    <?php wp_head();?>
+</head>
+<body <?php body_class('nagel');?>>
+    <?php wp_body_open(); ?>
+    <h1> hello</h1>
+    <?php wp_footer();?>
+</body>
+</html>
