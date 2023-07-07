b9848dfbb8c8052dfc16a7020250eaacf3711925
wingo-dev <rukshanlancer@gmail.com>
2023-07-07 14:12:33 +0800

    login page

--- wp-content/themes/nagel/includes/enqueue.php
+++ wp-content/themes/nagel/includes/enqueue.php
@@ -1,18 +1,25 @@
 <?php
 
 function u_enqueue(){
-    // Enqueue Poppins font
+   
     wp_enqueue_style( 'poppins-font', 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap' );
-    // Enqueue Raleway Semi font
+   
     wp_enqueue_style( 'raleway-semi-font', 'https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap' );
     
     wp_register_style(
         'u_css',
         get_theme_file_uri('assets/custom.css'),
-        array(), // Dependencies. In this case, none.
-        null // Version number. Null, in this case.
+        array(), 
+        null 
+    );
+    wp_register_style(
+        'fontawesome',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+        array(),
+        null
     );
     wp_enqueue_style('poppins-font');
     wp_enqueue_style('raleway-semi-font');
     wp_enqueue_style('u_css');
+    wp_enqueue_style('fontawesome');
 }
